604caa480fe53d13bd691e085434bd8ac215f9db
theiceoan <87422366+UP2028963@users.noreply.github.com>
2025-03-09 21:21:41 +0000

    Updates

--- client/src/pages/quiz.tsx
+++
@@ -1,9 +0,0 @@
-import { QuizFlow } from "@/components/QuizFlow";
-
-export default function Quiz() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-gradient-to-b from-background to-muted p-4">
-      <QuizFlow />
-    </div>
-  );
-}
